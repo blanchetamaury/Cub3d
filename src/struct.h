1176c9ba2d57f867435afee8182763f3fb18cc04
rgodet <rgodet@student.42.fr>
2025-07-08 13:52:44 +0200

    fix: Update structure Co-authored-by: amblanch <amblanch@students.42.fr>

--- src/struct.h
+++ src/struct.h
@@ -6,7 +6,7 @@
 /*   By: amblanch <amblanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 14:58:27 by amblanch          #+#    #+#             */
-/*   Updated: 2025/06/30 16:14:46 by amblanch         ###   ########.fr       */
+/*   Updated: 2025/07/08 13:39:28 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,27 @@ typedef struct s_texture
 
 typedef struct s_graphics
 {
-	///    
+	mlx_context	init;
+	mlx_window	window;
 }				t_graphics;
 
 typedef struct s_player
 {
 	int			x;
 	int			y;
-	///    
+	float			pos_x;
+	float			pos_y;
+	int			rad;
+	int			angle;
 }				t_player;
 
 typedef struct s_map
 {
-	///
     int         fd_map;
 	char		*name;
 	char		**map;
+	int			size;
+	int			cap;
 }				t_map;
 
 typedef struct s_game
@@ -57,6 +62,7 @@ typedef struct s_game
 	t_graphics	*graphics;
 	t_map		*map;
 	t_player	*player;
+	int			key[300];
 } 				t_game;
 
 #endif
