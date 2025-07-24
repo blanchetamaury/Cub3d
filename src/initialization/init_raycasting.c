e7c41baa50af21c84c09ddda2a3cfb4aed5440b4
rgodet <rgodet@student.42.fr>
2025-07-24 12:42:19 +0200

    feat: Shorten battery consumption

--- src/initialization/init_raycasting.c
+++ src/initialization/init_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: amblanch <amblanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 10:33:46 by amblanch          #+#    #+#             */
-/*   Updated: 2025/07/23 15:43:45 by amblanch         ###   ########.fr       */
+/*   Updated: 2025/07/24 12:16:49 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ t_raycasting	*init_raycasting(void)
 	ray = NULL;
 	ray = ft_calloc(1, sizeof(t_raycasting));
 	ray->fov = 70;
-	ray->light = LIGHT_ON;
+	ray->light = LIGHT_OFF;
 	return (ray);
 }
