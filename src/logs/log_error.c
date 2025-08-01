9608b1fa660457b9fdd6b73bf175a7d7c9fd2167
rgodet <rgodet@student.42.fr>
2025-08-01 10:17:17 +0200

    fix: Error message ":"

--- src/logs/log_error.c
+++ src/logs/log_error.c
@@ -6,7 +6,7 @@
 /*   By: rgodet <rgodet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 08:55:14 by rgodet            #+#    #+#             */
-/*   Updated: 2025/08/01 10:00:04 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/08/01 10:17:03 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	log_error(const char *message)
 {
 	int	len;
 
-	len = write(2, "\e[31;1mError :\e[0m\n", 20);
+	len = write(2, "\e[31;1mError\e[0m\n", 20);
 	if (message)
 		len += write(2, message, ft_strlen(message));
 	else
