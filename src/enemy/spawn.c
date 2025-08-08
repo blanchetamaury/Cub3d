896311fd7ad85df2ec872d00fab6f116a5a5cacc
rgodet <rgodet@student.42.fr>
2025-08-08 10:43:59 +0200

    fix: enemy spawn at invalid position on 3x3maps

--- src/enemy/spawn.c
+++ src/enemy/spawn.c
@@ -6,7 +6,7 @@
 /*   By: rgodet <rgodet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 10:56:38 by rgodet            #+#    #+#             */
-/*   Updated: 2025/07/29 12:36:07 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/08/08 09:54:32 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@ static void	set_bot_position(t_bot *bot, t_game *game)
 	max_x = ft_strlen(game->map->map[(int)game->player->pos_y]);
 	if (rand() % 2 == 0)
 		bot->pos_x = max(0, (int)game->player->base_pos_x - rand()
-				% ((int)game->player->base_pos_x - 1));
+				% ((int)game->player->base_pos_x));
 	else
 		bot->pos_x = max(max_x - 1, (int)game->player->base_pos_x + rand()
 				% (max_x - (int)game->player->base_pos_x));
 	if (rand() % 2 == 0)
 		bot->pos_y = max(0, (int)game->player->base_pos_y - rand()
-				% ((int)game->player->base_pos_y - 1));
+				% ((int)game->player->base_pos_y));
 	else
 		bot->pos_y = max(game->map->size - 1,
 				(int)game->player->base_pos_y + rand()
 				% ((game->map->size - 1) - (int)game->player->base_pos_y));
+	if (bot->pos_x > max_x - 1 || bot->pos_x < 0)
+		bot->pos_x = max_x - 1;
+	if (bot->pos_y > game->map->size - 1 || bot->pos_y < 0)
+		bot->pos_y = game->map->size - 1;
 }
 
 void	spawn_enemy(t_game *game)
